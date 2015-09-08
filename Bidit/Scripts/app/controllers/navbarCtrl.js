0016a8cd481b14be8191d3428d071a6e7fd9302e
dandan53 <dandan53@gmail.com>
2015-09-08 17:06:32 +0300

    login

---
+++ Bidit/Scripts/app/controllers/navbarCtrl.js
@@ -0,0 +1,12 @@
+﻿app.controller('NavbarCtrl', function ($scope, $location, userDataService) {
+
+    $scope.login = function () {
+        if (!userDataService.isLoggedIn()) {
+            $location.path('/login/');
+        } else {
+            alert('אתה כבר מחובר למערכת');
+        }
+    };
+
+    $scope.user = userDataService.getUserData();
+});
